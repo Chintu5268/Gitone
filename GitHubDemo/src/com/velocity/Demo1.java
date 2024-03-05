f7f83fd64a0e40c5096d719dc8dc20c4a66e03ed
Sandesh Sarade <Saradesandesh@gmail.com>
2024-03-05 15:50:44 +0530

    added arraylist with integer code for city

--- GitHubDemo/src/com/velocity/Demo1.java
+++ GitHubDemo/src/com/velocity/Demo1.java
@@ -1,9 +1,16 @@
 package com.velocity;
 
+import java.util.ArrayList;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Sandesh");
+		ArrayList aList = new ArrayList();
+		aList.add(45);
+		aList.add(45);
+		aList.add(45);
+		System.out.println(aList);
 
 	}
 
---
+++ GitHubDemo/src/com/velocity/Demo2.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(52);
+		list.add(42);
+		list.add(13);
+		System.out.println(list);
+
+	}
+
+}
